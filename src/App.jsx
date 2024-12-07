defe33ef4877907d1fba8667b185ad975f78903c
Alaaeldinabosamra <alaaeldinabosamra@gmail.com>
2024-12-07 12:06:07 +0200

    contact update

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Hero from "./sections/Hero.jsx";
 import About from "./sections/About.jsx";
 import Particle from "./components/Particle.jsx";
 import Projects from "./sections/Projects.jsx";
+import Footer from "./sections/Footer.jsx";
+import Contact from "./sections/Contact.jsx";
 function App() {
   return (
     <main className="max-w-7xl mx-auto relative">
@@ -18,6 +20,8 @@ function App() {
       <Projects />
       <VideoCarousel />
       <Geine />
+      <Contact />
+      <Footer />
     </main>
   );
 }
---
+++ src/sections/Contact.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import facebook from "../../public/assets/facebook.png";
+import instagram from "../../public/assets/instagram.png";
+import linkedin from "../../public/assets/linkedin.png";
+import github from "../../public/assets/github2.png";
+import Lottie from "lottie-react";
+import contact from "../../public/assets/mail.json";
+
+const Contact = () => {
+  return (
+    <section id="contact" className="relative relative py-10 px-5 md:px-0">
+      <div className="mb-16 max-w-7xl mx-auto">
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <div className="md:w-1/2 mb-8 ml-2 md:mb-0">
+            <h2 className="text-3xl font-bold mb-3 text-blue-500">
+              Get in Touch
+            </h2>
+            <p className="mb-4 text-white/85">
+              I'm always open to new opportunities and collaboration. Feel free
+              to reach out!
+            </p>
+            <div className="flex space-x-4">
+              <a
+                href="#"
+                className="text-foreground/60 hover:text-foreground/80"
+              >
+                <img src={facebook} alt="" className="h-6 w-6" />
+              </a>
+              <a
+                href="#"
+                className="text-foreground/60 hover:text-foreground/80"
+              >
+                <img src={instagram} alt="" className="h-6 w-6" />
+              </a>
+              <a
+                href="#"
+                className="text-foreground/60 hover:text-foreground/80"
+              >
+                <img src={github} alt="" className="h-6 w-6" />
+              </a>
+            </div>
+            <Lottie
+              animationData={contact}
+              className="w-[350px] mx-auto lg:w-[500px]"
+            />
+          </div>
+          <form className="w-full md:w-1/2 bg-blue-500/50 backdrop-blur-lg rounded-lg border border-blue-300 shadow-lg shadow-blue-500 p-10">
+            <h1 className="text-white-600 text-4xl font-bold mb-7">
+              Contact Me
+            </h1>
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-white-600"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Full Name"
+                className="mt-1 p-2 block w-full rounded-md bg-black-200 border border-black-300 text-gray-400 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-white-600"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Email"
+                className="mt-1 p-2 block w-full rounded-md bg-black-200 border border-black-300 text-gray-400 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="message"
+                className="block text-sm font-medium text-white-600"
+              >
+                Message
+              </label>
+              <textarea
+                id="message"
+                placeholder="Enter Your Message"
+                className="mt-1 p-2 block w-full rounded-md bg-black-200 border border-black-300 text-gray-400 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              />
+            </div>
+            <button className="bg-blue-500 text-white px-3 py-2 rounded-lg">
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
