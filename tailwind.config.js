a72e22dc13ed0971fa57bef6123b398efb2e2a89
Alaaeldinabosamra <alaaeldinabosamra@gmail.com>
2024-12-07 08:23:37 +0200

    assets and extensions

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        "sm-custom": "350px", // Custom screen size for 600px
+      },
+      fontFamily: {
+        generalsans: ["General Sans", "sans-serif"],
+      },
+      colors: {
+        black: {
+          DEFAULT: "#000",
+          100: "#010103",
+          200: "#0E0E10",
+          300: "#1C1C21",
+          500: "#3A3A49",
+          600: "#1A1A1A",
+          700: "#010001",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+          800: "#E4E4E6",
+          700: "#D6D9E9",
+          600: "#AFB0B6",
+          500: "#62646C",
+        },
+      },
+      backgroundImage: {
+        terminal: "url('/assets/terminal.png')",
+      },
+    },
+  },
+  plugins: [],
+};
